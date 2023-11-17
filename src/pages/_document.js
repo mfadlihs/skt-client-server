db69fc72a7fdd3e94bf4043d34d7c9b4ad87cc22
mfadlihs <winnerfadli999@gmail.com>
2023-11-17 13:59:56 +0700

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { ToastContainer } from "react-toastify";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-white">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
